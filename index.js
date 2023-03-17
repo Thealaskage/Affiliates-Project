6d48b68dc54d1db8e99a91138e76c4cca562220f
Jorge Ponce <u202120471@upc.edu.pe>
2023-03-16 20:59:17 -0500

    update 0.1

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ const inputs = document.querySelectorAll("#affillier-form input")
 //     });
 // });
 const expressions = {
-	firstName: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-    lastName: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	firstName: /^[a-zA-ZÀ-ÿ\s]{1,16}$/, // Letras y espacios, pueden llevar acentos.
+    lastName: /^[a-zA-ZÀ-ÿ\s]{1,16}$/, // Letras y espacios, pueden llevar acentos.
     password: /^.{4,12}$/, // 4 a 12 digitos.
 	DNI: /^\d{8}$/, // 8 numeros.
 	phone: /^\d{9}$/ // 9 numeros.
@@ -42,7 +42,7 @@ const validateForm = (e) =>{
             validateField(expressions.password, e.target, "password");
         break;
         case "password2":
-            validateField(expressions.password, e.target, "password2");
+            validatePassword2();
         break;
         case "phone":
             validateField(expressions.phone, e.target, "phone");
@@ -66,6 +66,24 @@ const validateField = (expression, input, field) =>{
         document.querySelector(`#grupo__${field} i`).classList.add("fa-times-circle");
         document.querySelector(`#grupo__${field} i`).classList.remove("fa-check-circle");
         document.querySelector(`#grupo__${field} .formulario__input-error`).classList.add("formulario__input-error-activo");
+    }
+}
+const validatePassword2 = () => {
+    const inputPassword1 = document.getElementById("password");
+    const inputPassword2 = document.getElementById("password2");
+
+    if(inputPassword1.value !== inputPassword2.value){
+        document.getElementById(`grupo__password2`).classList.add("formulario__grupo-incorrecto");
+        document.getElementById(`grupo__password2`).classList.remove("formulario__grupo-correcto");
+        document.querySelector(`#grupo__password2 i`).classList.add("fa-times-circle");
+        document.querySelector(`#grupo__password2 i`).classList.remove("fa-check-circle");
+        document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add("formulario__input-error-activo");
+    }else{
+        document.getElementById(`grupo__password2`).classList.remove("formulario__grupo-incorrecto");
+        document.getElementById(`grupo__password2`).classList.add("formulario__grupo-correcto");
+        document.querySelector(`#grupo__password2 i`).classList.add("fa-check-circle");
+        document.querySelector(`#grupo__password2 i`).classList.remove("fa-times-circle");
+        document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove("formulario__input-error-activo");
     }
 }
 inputs.forEach((input) => {
