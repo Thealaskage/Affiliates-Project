df7009dd0ab2df797b2f2d6269e073d396729f82
Jorge Ponce <u202120471@upc.edu.pe>
2023-06-07 15:22:06 -0500

    first commit

--- firebase.js
+++ firebase.js
@@ -1,35 +1,59 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
-import { 
-    collection,
-    getFirestore,
-    addDoc,
-    getDocs,
-    onSnapshot
-} from "https://www.gstatic.com/firebasejs/9.17.2/firebase-firestore.js"
+import {
+  collection,
+  getFirestore,
+  addDoc,
+  getDocs,
+  onSnapshot,
+  where,
+  query,
+} from "https://www.gstatic.com/firebasejs/9.17.2/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-    
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-apiKey: "AIzaSyCanGXtePHqKmmlTXJXqf3VgCRUoJfCyZs",
-authDomain: "affilliers.firebaseapp.com",
-projectId: "affilliers",
-storageBucket: "affilliers.appspot.com",
-messagingSenderId: "566986456653",
-appId: "1:566986456653:web:b63e91a53891500e29a604"
+  apiKey: "AIzaSyBXJOEaEHdulFsBmikU_CsMm4pPYrdZXec",
+  authDomain: "pizzeria-delicia-60823.firebaseapp.com",
+  projectId: "pizzeria-delicia-60823",
+  storageBucket: "pizzeria-delicia-60823.appspot.com",
+  messagingSenderId: "467980380873",
+  appId: "1:467980380873:web:8790a57c73b76537403b18",
+  measurementId: "G-CKJT0TPVF3",
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore();
 
-export const saveAffillier = (firstName, lastName, password, phone, DNI) => {
-    addDoc(collection(db, "users"), {firstName, lastName, password, phone, DNI});
-}
+export const saveAffillier = async (firstName, lastName, password, phone, DNI) => {
+  try {
+    const docRef = await addDoc(collection(db, "ambassadors"), { firstName, lastName, password, phone, DNI });
+    return docRef;
+  } catch (error) {
+    console.error(error);
+    throw new Error("Hubo un error al guardar el afiliado.");
+  }
+};
 
-export const getAffilliers = () => getDocs(collection(db, "users"));
+export const getAffilliers = () => getDocs(collection(db, "ambassadors"));
 
-export const onGetAfilliers = (callback) => onSnapshot(collection(db, "users"), callback);
+export const onGetAfilliers = (callback) =>
+  onSnapshot(collection(db, "ambassadors"), callback);
 
+// Función para verificar si el número de teléfono ya existe en la base de datos
+export const checkIfPhoneExists = async (phone) => {
+  const querySnapshot = await getDocs(
+    query(collection(db, "ambassadors"), where("phone", "==", phone))
+  );
+  return querySnapshot.size > 0;
+};
+
+// Función para verificar si el DNI ya existe en la base de datos
+export const checkIfDNIExists = async (DNI) => {
+  const querySnapshot = await getDocs(
+    query(collection(db, "ambassadors"), where("DNI", "==", DNI))
+  );
+  return querySnapshot.size > 0;
+};
--- index.js
+++ index.js
@@ -1,30 +1,17 @@
-import {saveAffillier, getAffilliers, onGetAfilliers} from "./firebase.js"
+import {saveAffillier, getAffilliers, checkIfPhoneExists, checkIfDNIExists} from "./firebase.js"
 
 const affillierForm = document.getElementById("affillier-form");
 const inputs = document.querySelectorAll("#affillier-form input")
-//Show--Data
-// const afilliersContainer = document.getElementById("affilliers-container");
-// window.addEventListener("DOMContentLoaded", async () => {
-//     onGetAfilliers((querySnapshot) => {
-//         let content = " ";
-//         querySnapshot.forEach(doc => {//QuerySnapshot Datos que existen en este momento
-//             const task = doc.data();
-//             content += ` 
-//                 <div>
-//                     <h3>${task.firstName}</h3>
-//                     <h3>${task.lastName}</h3>
-//                     <h3>${task.phone}</h3>
-//                     <h3>${task.DNI}</h3>
-//                     <button>Delete</button>
-//                     <br/>
-//                 </div>
-//             `
-//         });
-//         afilliersContainer.innerHTML = content;
-//     });
-// });
+
+const fields = {
+    firstName: false,
+    lastName: false,
+    password: false,
+    phone: false,
+    DNI: false
+}
 const expressions = {
-	firstName: /^[a-zA-ZÀ-ÿ\s]{1,16}$/, // Letras y espacios, pueden llevar acentos.
+    firstName: /^[a-zA-ZÀ-ÿ\s]{1,16}$/, // Letras y espacios, pueden llevar acentos.
     lastName: /^[a-zA-ZÀ-ÿ\s]{1,16}$/, // Letras y espacios, pueden llevar acentos.
     password: /^.{4,12}$/, // 4 a 12 digitos.
 	DNI: /^\d{8}$/, // 8 numeros.
@@ -40,9 +27,10 @@ const validateForm = (e) =>{
         break;
         case "password":
             validateField(expressions.password, e.target, "password");
+            validateField2()
         break;
         case "password2":
-            validatePassword2();
+            validateField2()
         break;
         case "phone":
             validateField(expressions.phone, e.target, "phone");
@@ -50,6 +38,8 @@ const validateForm = (e) =>{
         case "DNI":
             validateField(expressions.DNI, e.target, "DNI");
         break;
+        
+
     }
 
 }
@@ -60,45 +50,95 @@ const validateField = (expression, input, field) =>{
         document.querySelector(`#grupo__${field} i`).classList.add("fa-check-circle");
         document.querySelector(`#grupo__${field} i`).classList.remove("fa-times-circle");
         document.querySelector(`#grupo__${field} .formulario__input-error`).classList.remove("formulario__input-error-activo");
+        fields[field] = true;
     }else{
         document.getElementById(`grupo__${field}`).classList.add("formulario__grupo-incorrecto");
         document.getElementById(`grupo__${field}`).classList.remove("formulario__grupo-correcto");
         document.querySelector(`#grupo__${field} i`).classList.add("fa-times-circle");
         document.querySelector(`#grupo__${field} i`).classList.remove("fa-check-circle");
         document.querySelector(`#grupo__${field} .formulario__input-error`).classList.add("formulario__input-error-activo");
+        fields[field] = false;
     }
 }
-const validatePassword2 = () => {
-    const inputPassword1 = document.getElementById("password");
-    const inputPassword2 = document.getElementById("password2");
+const validateField2 = () => {
+	const inputPassword1 = document.getElementById('password');
+	const inputPassword2 = document.getElementById('password2');
 
-    if(inputPassword1.value !== inputPassword2.value){
-        document.getElementById(`grupo__password2`).classList.add("formulario__grupo-incorrecto");
-        document.getElementById(`grupo__password2`).classList.remove("formulario__grupo-correcto");
-        document.querySelector(`#grupo__password2 i`).classList.add("fa-times-circle");
-        document.querySelector(`#grupo__password2 i`).classList.remove("fa-check-circle");
-        document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add("formulario__input-error-activo");
-    }else{
-        document.getElementById(`grupo__password2`).classList.remove("formulario__grupo-incorrecto");
-        document.getElementById(`grupo__password2`).classList.add("formulario__grupo-correcto");
-        document.querySelector(`#grupo__password2 i`).classList.add("fa-check-circle");
-        document.querySelector(`#grupo__password2 i`).classList.remove("fa-times-circle");
-        document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove("formulario__input-error-activo");
-    }
+	if(inputPassword1.value !== inputPassword2.value){
+		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__password2 i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__password2 i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add('formulario__input-error-activo');
+		fields['password'] = false;
+	} else {
+		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__password2 i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__password2 i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		fields['password'] = true;
+	}
 }
+
 inputs.forEach((input) => {
     input.addEventListener("keyup", validateForm);
     input.addEventListener("blur", validateForm)
 })
 
-affillierForm.addEventListener("submit", (e) => {
+affillierForm.addEventListener("submit", async (e) => {
     e.preventDefault();
+    const terms = document.getElementById("terms");
+    const phone = affillierForm["phone"];
+    const DNI = affillierForm["DNI"];
     const firstName = affillierForm["first-name"];
     const lastName = affillierForm["last-name"];
     const password = affillierForm["password"];
-    const phone = affillierForm["phone"];
-    const DNI = affillierForm["DNI"];
-    
-    saveAffillier(firstName.value, lastName.value, password.value, phone.value, DNI.value);
+    const password2 = affillierForm["password2"];
 
+
+    if (firstName.value.trim() === "" || lastName.value.trim() === "" || password.value.trim() === "" || password2.value.trim() === "" || phone.value.trim() === "" || DNI.value.trim() === "" || !terms.checked) {
+        document.getElementById("formulario__message").classList.add("formulario__mensaje-activo");
+        setTimeout(() => {
+          document.getElementById("formulario__message").classList.remove("formulario__mensaje-activo");
+        }, 5000); // Ocultar el mensaje después de 5 segundos
+        return;
+      }
+
+    const phoneExists = await checkIfPhoneExists(phone.value);
+    const DNIExists = await checkIfDNIExists(DNI.value);
+
+    if (phoneExists || DNIExists) {
+        alert("El afiliado ya existe en la base de datos");
+    } else {
+        try {
+            await saveAffillier(firstName.value, lastName.value, password.value, phone.value, DNI.value);
+            alert("Afiliado guardado correctamente");
+            affillierForm.reset();
+            document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+            setTimeout(() => {
+                document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+            }, 5000);
+            document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+                icono.classList.remove('formulario__grupo-correcto');
+            });
+        } catch (error) {
+            console.error(error);
+            alert("Hubo un error al guardar el afiliado. Por favor, inténtalo nuevamente.");
+        }
+    }
 });
+
+
+
+
+
+
+
+
+  
+  
+  
+  
+  
+  
